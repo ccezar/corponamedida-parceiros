cb610fbe8c5dc81669c1f1ff3cac4bd6260a197f
Caio Cezar Lopes dos Santos <c.cezar.lopes@gmail.com>
2015-06-04 10:37:21 -0300

    Commit inicial

---
+++ Parceiros/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Parceiros
+//
+//  Created by Caio Cezar Lopes dos Santos on 04/06/15.
+//  Copyright (c) 2015 Corpo na Medida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
